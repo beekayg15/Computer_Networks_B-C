cac1d086a754349b0a38ae91d44a2e1a20bc7d9b
BarathKumar <barathganeshkumar2002@gmail.com>
2021-08-20 03:55:23 +0530

    basic game completed

---
+++ bcgame.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define PORT 1515
+
+void openGame(int sockfd);
+int getch(void);
+void instructions(int sockfd);
+void startGame(int sockfd);
+
+int main(){
+    socklen_t addr_len;
+    int client_socket;
+    struct sockaddr_in server_address;
+
+    client_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+    if(client_socket < 0) {
+        printf("Error While Creating Socket!!\n");
+        exit(0);
+
+    } else {
+        printf("Socket Creation Successful!!\n");
+
+    }
+
+    addr_len = sizeof(server_address);
+    bzero(&server_address, addr_len);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if(connect(client_socket, (struct sockaddr*)&server_address, addr_len) != 0){
+        printf("Connection to Server Failed!!\n");
+        exit(0);
+
+    } else {
+        printf("Connection to Server Successful!!\n");
+
+    }
+
+    printf("Press any Key to Continue......");
+    getch();
+
+    openGame(client_socket);
+
+    return 0;
+
+}
+
+void openGame(int sockfd) {
+    system("clear");
+
+    printf("\n\n\n\tBULLS AND COWS");
+    printf("\n\n\t\t1.Start Game");
+    printf("\n\n\t\t2.Instructions");
+    printf("\n\n\t\t3.Quit");
+    printf("\n\n\tPress the corresponding number to continue......");
+
+    char choice = getch();
+
+    switch(choice) {
+        case '1': startGame(sockfd); break;
+        case '2': instructions(sockfd); break;
+        case '3': exit(0);
+        default: openGame(sockfd);
+
+    }
+
+    return;
+
+}
+
+void startGame(int sockfd) {
+    system("clear");
+    
+    char buffer[1024];
+    int n = 0;
+    bzero(buffer, 1024);
+    
+    printf("\n\n\n\t\tGo on!! Make a Guess : ");
+    while((buffer[n++] = getchar()) != '\n');
+
+    send(sockfd, buffer, 1024, 0);
+
+    if(strncmp("exit", buffer, 4) == 0) {
+        openGame(sockfd);
+
+    }
+
+    bzero(buffer, 1024);
+    recv(sockfd, buffer, 1024, 0);
+
+    printf("\n\n\t\t%s", buffer);
+
+    if(buffer[0] == 'C') {
+        printf("\n\n\t\tPress any Key to Conitnue......");
+        getch();
+
+        openGame(sockfd);
+
+    } else {
+        printf("\n\n\t\tPress any Key to Conitnue......");
+        getch();
+
+        startGame(sockfd);
+
+    }
+
+    return;
+
+}
+
+void instructions(int sockfd) {
+    system("clear");
+
+    printf("\n\tInstructions");
+    printf("\n\n'Bulls and Cows' is an old Code-Breaking mind game in which the player is engaged to figure out a 4 digit code by the trail and error method. The number of cows represent the digits that were guessed and present in the actual code but not in the exact position. Example:");
+    printf("\n\n\t\t3564  (Guessed Number)");
+    printf("\n\t\t/  \\ ");
+    printf("\n\t\t5986  (Actual Code)");
+    printf("\n\nThus the number of cows is 2. The number of bulls represent the digits that were guessed and present in the actual code and also the exact same position. Example:");
+    printf("\n\n\t\t4365  (Guessed Number)");
+    printf("\n\t\t |");
+    printf("\n\t\t9312  (Actual Code)");
+    printf("\n\nTherefor the number of bulls is 1. Thus the code can be found by analyzing the number of bulls and cows");
+    printf("\n\nHave Fun!!!!");
+
+    printf("\n\nPress any Key to continue......");
+    getch();
+
+    openGame(sockfd);
+
+    return;
+
+}
+
+int getch(void) {
+    struct termios oldattr, newattr;
+    int ch;
+
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+
+    return ch;
+
+}
---
+++ bcserver.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#define PORT 1515
+
+void initiateGame(int sockfd);
+int numberOfBulls(char guess[], char num[]);
+int numberOfCows(char guess[], char num[]);
+int valid(char guess[]);
+
+int main() {
+    socklen_t addr_len;
+    struct sockaddr_in server_address;
+    int server_socket;
+
+    server_socket = socket(PF_INET, SOCK_STREAM, 0);
+
+    if(server_socket < 0) {
+        printf("Error While Creating Socket!!\n");
+        exit(0);
+
+    } else {
+        printf("Socket Creation Successful!!\n");
+
+    }
+
+    addr_len = sizeof(server_address);
+    bzero(&server_address, addr_len);
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(PORT);
+
+    if(bind(server_socket, (struct sockaddr*)&server_address, addr_len) != 0) {
+        printf("Binding Failed!!\n");
+        exit(0);
+
+    } else {
+        printf("Binding Successful!!\n");
+
+    }
+
+    if(listen(server_socket, 2) != 0) {
+        printf("Unable to Listen to Clients!!\n");
+        exit(0);
+
+    } else {
+        printf("Waiting for Clients!!\n");
+
+    }
+
+    int client_socket;
+    struct sockaddr_in client_address;
+
+    client_socket = accept(server_socket, (struct sockaddr*)&client_address, &addr_len);
+
+    if(client_socket < 0) {
+        printf("Error in Establishing Connection with the Client!!\n");
+        exit(0);
+
+    } else {
+        printf("Connection with Client Established Successfully!!\n");
+
+    }
+
+    initiateGame(client_socket);
+
+    return 0;
+
+}
+
+void initiateGame(int sockfd) {
+    int number_of_turns = 0;
+    char buffer[1024];
+
+    char number[4] = "    ";
+    int count = 0;
+
+    while(count < 4) {
+        int flag = 0;
+        int r = rand() % 9 + 49;
+        
+        for(int index=0; index<count; index++) {
+            if((char)r == number[index]) {
+                flag = 1;
+
+            }
+
+        }
+
+        if(flag == 0) {
+            number[count] = (char)r;
+            count++;
+
+        }
+
+    }
+
+    printf("%c%c%c%c\n", number[0], number[1], number[2], number[3]);
+
+    while(1) {
+        bzero(buffer, 1024);
+
+        recv(sockfd, buffer, 1024, 0);
+        printf("Turn Number : %d, %s",number_of_turns, buffer);
+
+        if(strncmp("exit", buffer, 4) == 0) {
+            printf("The client has left the current game!!\n");
+            initiateGame(sockfd);
+
+        }
+
+        number_of_turns++;
+
+        int bulls = numberOfBulls(buffer, number);
+        int cows = numberOfCows(buffer, number);
+        
+        printf("Number of Bulls: %d\n", bulls);
+        printf("Number of Cows: %d\n", cows);
+
+        if(bulls == 4) {
+            bzero(buffer, 1024);
+
+            char temp[] = "Congarts!! Number of Turns Taken: _\n";
+
+            for(int z=0; temp[z]!='\n'; z++) {
+                buffer[z] = temp[z];
+
+            }
+
+            char s[4]; 
+            sprintf(s,"%d", number_of_turns);
+            buffer[34] = s[0];
+
+            for(int z=1; z<5 && s[z-1]!='\0'; z++) {
+                if(s[z] == '\0') {
+                    buffer[z+34] = '\n';
+
+                } else {
+                    buffer[z+34] = s[z];
+
+                }
+
+            }
+
+            send(sockfd, buffer, 1024, 0);
+
+            initiateGame(sockfd);
+
+        }
+
+        bzero(buffer, 1024);
+
+        char temp[] = "Bulls: _; Cows: _; In turn: _\n\0";
+
+        for(int z=0; temp[z]!='\0'; z++) {
+            buffer[z] = temp[z];
+
+        }
+
+        buffer[7] = (char)(bulls + 48);
+        buffer[16] = (char)(cows + 48);
+
+        char s[4]; 
+        sprintf(s,"%d", number_of_turns);
+        buffer[28] = s[0];
+
+        for(int z=1; z<5 && s[z-1]!='\0'; z++) {
+            if(s[z] == '\0') {
+                buffer[z+28] = '\n';
+
+            } else {
+                buffer[z+28] = s[z];
+
+            }
+
+        }
+
+        send(sockfd, buffer, 1024, 0);
+
+    }
+
+    return;
+
+}
+
+int numberOfBulls(char guess[], char num[]) {
+    int bulls = 0;
+
+    for(int index=0; index<4; index++) {
+        if(guess[index] == num[index]) {
+            bulls++;
+
+        }
+
+    }
+
+    return bulls;
+
+}
+
+int numberOfCows(char guess[], char num[]) {
+    int cows = 0;
+
+    for(int index1=0; index1<4; index1++) {
+        for(int index2=0; index2<4; index2++) {
+            if((guess[index1] == num[index2]) && (index1 != index2)) {
+                cows++;
+
+            }
+
+        }
+
+    }
+
+    return cows;
+
+}
